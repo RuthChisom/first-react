0fa3484508665fa490fd96312a8dfb3c60c6be08
Ruth Chisom <ndukwe100805043@gmail.com>
2022-05-24 22:57:16 +0100

    React Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import FComp from './Components/FunctionalComp'; //I renamed the function here, that's allowed
+import ClassComp from './Components/ClassComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // multiple html tags MUST be enclosed in a div tag
+    <div>
+      <h1>Hello! I'm Ruth Chisom.</h1>
+      <h1>I'm an Angular Developer, learning React.</h1>
+      <FComp/>
+      <ClassComp/>
     </div>
-  );
+    )
 }
 
 export default App;
